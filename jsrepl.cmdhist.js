5fd4e602e7bef6dfb27c68345b8f59c34063f326
default_git_name <default_git_email>
2012-02-17 09:05:35 +0000

    Extracted command history store into its own module.

---
+++ jsrepl.cmdhist.js
@@ -0,0 +1,48 @@
+// ** Command history store and scroll logic **
+//
+var jsrepl = jsrepl || {};
+jsrepl.cmdhist = function() {
+	var currentHistoryIndex = 0;
+	var historyCommands = [];
+
+	function scrollHistory(historyOffset) {
+		var newHistoryIndex =
+			currentHistoryIndex + historyOffset;
+
+		if(newHistoryIndex < 0) {
+			newHistoryIndex = 0;
+		}
+		else if(newHistoryIndex >= historyCommands.length) {
+			newHistoryIndex = historyCommands.length -1;
+		}
+
+		currentHistoryIndex = newHistoryIndex;
+
+		var historyCommand =
+			historyCommands[currentHistoryIndex];
+
+		if(	historyCommand === undefined ||
+			historyCommand === null) {
+			historyCommand = "";
+		}
+
+		return historyCommand;
+	}
+
+	function resetHistoryIndex() {
+		currentHistoryIndex =
+			historyCommands.length;
+	}
+
+	function pushHistoryCommand(commandStr) {
+		historyCommands.push(commandStr);		
+	}
+
+	var pub = {
+		scrollHistory      : scrollHistory,
+		resetHistoryIndex  : resetHistoryIndex,
+		pushHistoryCommand : pushHistoryCommand
+	};
+
+	return pub;
+}();
